aee343ef7a179da78d6c9c33a421c571b46183e6
jackelizeth <jackeline-chavez-m@hotmail.com>
2018-05-25 00:28:35 -0500

    estructura de HTML y funciones js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function cifrar (){
+    //document.write(" hola mundo");
+    const alfb =[ ]
+//captura el valor del texto del elemento por el Id = texto
+let text1 = document.getElementById('texto').value;
+ 
+
+//asigna el valor capturado hacia la etiqueta de Id = texto_cifrado
+document.getElementById('texto_cifrado').innerHTML= text1;
+
+  }
+
+
+  function decifrar (){
+  
+  
+  }
+
+
+
+
