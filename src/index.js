6df4fca4bae0a90c07656acf2ad8f99f592ed346
jackelizeth <jackeline-chavez-m@hotmail.com>
2018-05-25 22:05:03 -0500

    cmbiando name de mensaje2 a resultado-2

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ function cifrar (){
 //captura el valor del texto del elemento por el Id = texto
 let text1 = document.getElementById('texto').value;
  
-
 //asigna el valor capturado hacia la etiqueta de Id = texto_cifrado
 document.getElementById('texto_cifrado').innerHTML= text1;
-
   }
 
-
   function decifrar (){
-  
-  
+    
   }
 
 
