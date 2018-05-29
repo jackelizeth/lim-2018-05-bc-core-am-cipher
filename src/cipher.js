16d8b196b4d4040243ff3f6bd2ad9e7aab09a0e4
jackelizeth <jackeline-chavez-m@hotmail.com>
2018-05-29 11:00:39 -0500

    agregando cambios en html y js y realizando el encode en cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,27 @@
+window.cipher = { 
 
-window.cipher = {
-  // ... 
-};
-  
-
+  Encode:  (string, offset) => { // metodo para cifrar
+ 
+ //alert(string);
+ 
+    let text_result ="";
+ 
+    let tamanio = string.length
+ 
+ 
+    for (let i=0; i<tamanio; i++){
+      let pos_Ascii=string.charCodeAt(i);
+ 
+      //alert(pos_Ascii)
+ 
+      let res_formula=(pos_Ascii-65+offset)%26+65;
+ 
+      text_result += String.fromCharCode(res_formula);
+ 
+      }
+      return text_result;
+ 
+    },
+ 
+  }
+ 
