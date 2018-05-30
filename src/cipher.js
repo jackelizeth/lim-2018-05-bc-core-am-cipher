7ca800e8db1d2102e207ae3192aceb15a3522d3e
jackelizeth <jackeline-chavez-m@hotmail.com>
2018-05-30 13:00:48 -0500

    subiendo js encode

--- src/cipher.js
+++ src/cipher.js
@@ -2,31 +2,36 @@ window.cipher = {
 
   encode:  (string, offset) => { // metodo para cifrar
  //alert(string);
-    let resultado ="";
-    
+    let resultado =" ";
+    let valorOffset = parseInt(offset) 
+
     for (let i = 0; i < string.length; i++){//lenght cuenta los caracteres del string
-      
-      let posicion_Ascii = string.charCodeAt(i); // el charcodeat muestra la equivalencia del codigo ascii
-
-    //alert(posicion_Ascii);
-
-      //charCodeAt (0)empieza con cero y cero es 65 = a la letra A
-      //charCodeAt (1)empieza con cero y cero es 65 = a la letra 
-
-      //alert(posicion_Ascii);
- 
-      let res_formula = (posicion_Ascii-65+parseInt(offset))%26+65;
-
+      let posicion_Ascii = string.toUpperCase().charCodeAt(i); // el charCodeAt para obtener el codigo ascii de 1 letra
+     //alert(posicion_Ascii);
+     //alert(posicion_Ascii);
+      let res_formula = (posicion_Ascii - 65 + (valorOffset)) %26 +65;
      //alert(res_formula);
- 
-      resultado += String.fromCharCode(res_formula);
-
+     resultado += String.fromCharCode(res_formula);//el fromCharCode para convertir el codigo ascii a 1 letra 
      // alert(resultado)
-
-      }
-      return resultado;
- 
-    },
- 
+    }
+    return resultado;
   }
+
+  decode:  (string, offset) => { // metodo para cifrar
+    //alert(string);
+       let resultado =" ";
+       let valorOffset = parseInt(offset) 
+       for (let i = 0; i < string.length; i++){//lenght cuenta los caracteres del string
+         let posicion_Ascii = string.toUpperCase().charCodeAt(i); // el charCodeAt para obtener el codigo ascii de 1 letra
+        //alert(posicion_Ascii);
+        //alert(posicion_Ascii);
+         let res_formula = (posicion_Ascii + 65 - (valorOffset)) %26 +65;
+        //alert(res_formula);
+        resultado += String.fromCharCode(res_formula);//el fromCharCode para convertir el codigo ascii a 1 letra 
+        // alert(resultado)
+       }
+       return resultado;
+     }   
+
+};
  
