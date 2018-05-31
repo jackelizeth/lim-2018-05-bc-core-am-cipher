376a20452213f3c872cbf0509e39f99d082c02ef
jackelizeth <jackeline-chavez-m@hotmail.com>
2018-05-31 09:57:43 -0500

    subiendo arreglos en js para que escribas con espasios

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,21 @@
 window.cipher = { 
   
   encode : (string , offset) => { 
-    // metodo para cifrar
-
     let resultado ="";
     let valorOffset = parseInt(offset) 
 
     for (let i = 0; i < string.length; i++){
       //lenght cuenta los caracteres del string
-      let posicion_Ascii = string.toUpperCase().charCodeAt(i); 
+      const posicion_Ascii = string.toUpperCase().charCodeAt(i); 
       // el charCodeAt para obtener el codigo ascii de 1 letra
      
-      let res_formula = (posicion_Ascii - 65 + (valorOffset)) %26 +65;
-    
-     resultado += String.fromCharCode(res_formula);//el fromCharCode para convertir el codigo ascii a 1 letra 
+        if (posicion_Ascii === 32){
+          resultado += " ";
+        }else{
+          const res_formula = (posicion_Ascii - 65 + (valorOffset)) %26 +65;
+            resultado += String.fromCharCode(res_formula);//el fromCharCode para convertir el codigo ascii a 1 letra 
+        }
+     
     
     }
     return resultado;
@@ -25,13 +27,15 @@ window.cipher = {
        let valorOffset = parseInt(offset) 
    
        for (let i = 0; i < string.length; i++){//lenght cuenta los caracteres del string
-         let posicion_Ascii = string.toUpperCase().charCodeAt(i); // el charCodeAt para obtener el codigo ascii de 1 letra
-       
-       
-         let res_formula = (posicion_Ascii + 65 - (valorOffset)) %26 +65;
+         const posicion_Ascii = string.toUpperCase().charCodeAt(i); // el charCodeAt para obtener el codigo ascii de 1 letra
+         if (posicion_Ascii === 32){
+          resultado += " ";
+        }else{
+        const res_formula = (posicion_Ascii + 65 - (valorOffset)) %26 +65;
        
         resultado += String.fromCharCode(res_formula);//el fromCharCode para convertir el codigo ascii a 1 letra 
-      
+        }
+        
        }
        return resultado;
      },
