c16db0da1de7c992bf01e2f30d6ba12004825104
jackelizeth <jackeline-chavez-m@hotmail.com>
2018-06-01 02:56:44 -0500

    subiendo comentarios en js y cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,44 +1,53 @@
-window.cipher = { 
-  
-  encode : (string , offset) => { 
-    let resultado ="";
-    let valorOffset = parseInt(offset) 
-
-    for (let i = 0; i < string.length; i++){
-      //lenght cuenta los caracteres del string
-      const posicion_Ascii = string.toUpperCase().charCodeAt(i); 
-      // el charCodeAt para obtener el codigo ascii de 1 letra
-     
-        if (posicion_Ascii === 32){
-          resultado += " ";
-        }else{
-          const res_formula = (posicion_Ascii - 65 + (valorOffset)) %26 +65;
-            resultado += String.fromCharCode(res_formula);//el fromCharCode para convertir el codigo ascii a 1 letra 
-        }
-     
-    
-    }
-    return resultado;
+window.cipher = {
+    // metodo encode (codifica) 
+    // string texto que se quiere cifrar
+    // offset el numero de posicion que se movera a la derecha
+  encode : (string, offset) => { 
+    let result = " ";
+    /* parseInt(string, base) : Convierte (parsea): de un string a numero
+       ejm el  parseInt("F", 16)  devuelve de resultado 15 */
+    let valueOffset = parseInt(offset) 
+    /* string.length : es para obtener la longitud de un string o cadena (cantidad los caracteres)
+       (la propiedad length de un objeto String devuelve la longitud de una string)
+       ejm string.length de "your name" la longitud del string es 9 */
+    for (let i = 0; i < string.length; i++) {
+    /* string.toUpperCase() = String.prototype.toUpperCase() : este método devuelve el string convertida
+       a mayúsculas.*/
+    /* el método string.charCodeAt(i) = String.prototype.charCodeAt() : devuelve un número "codigo ASCII"(Unicode)
+       del carácter de una letra - Unicode aportar un único punto de código para cada carácter - (i) si no se especifica,
+       su valor predeterminado es 0.*/
+      const code_Ascii = string.toUpperCase().charCodeAt(i); 
+      // 32 representa el espacio entre las letras
+      if (code_Ascii === 32) {
+        result += " ";
+      } else {
+          const result_formula = (code_Ascii - 65 + (valueOffset)) % 26 + 65;
+    // ejm: "A" su code es 65 entonces  (65 - 65 + (valueOffset = 2)) % 26 + 65 = 67 entonces... 
+    /* String.fromCharCode(num1, ..., numN) : Este método devuelve una "string" y no un objeto String "number".
+       del ejm anterior "A" su code es 65 aplicamos String.fromCharCode(67) nos devolvera la letra "c" */
+          result += String.fromCharCode(result_formula);
+    // (x = x + y)  del ejemplo anterior el result seria "c"
+      }
+   }
+   return result;
   },
-  
-  decode : (string , offset) => { // metodo para cifrar
-   
-       let resultado =" ";
-       let valorOffset = parseInt(offset) 
-   
-       for (let i = 0; i < string.length; i++){//lenght cuenta los caracteres del string
-         const posicion_Ascii = string.toUpperCase().charCodeAt(i); // el charCodeAt para obtener el codigo ascii de 1 letra
-         if (posicion_Ascii === 32){
-          resultado += " ";
-        }else{
-        const res_formula = (posicion_Ascii + 65 - (valorOffset)) %26 +65;
-       
-        resultado += String.fromCharCode(res_formula);//el fromCharCode para convertir el codigo ascii a 1 letra 
-        }
-        
-       }
-       return resultado;
-     },
+    // metodo decode (descodificar) 
+    // string texto que se quiere decifrar
+    // offset el numero de posicion que se movera a la izquierda
+  decode : (string, offset) => {
+    let result = " ";
+    let valueOffset = parseInt(offset) 
+    for (let i = 0; i < string.length; i++) {
+      const code_Ascii = string.toUpperCase().charCodeAt(i) ;
+      if (code_Ascii === 32) {
+        result += " ";
+      } else {
+          const result_formula = (code_Ascii + 65 - (valueOffset)) % 26 +65;
+          result += String.fromCharCode(result_formula);
+      }
+   }
+   return result;
+ },
 
 };
  
--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
-document.getElementById("Cifrando").addEventListener("click",function(event){
 
+let in_text = document.getElementById("ingresando_text");
+// captuta la etiqueta textarea del id "ingresando_text" donde ingresana informacion
+let in_position = document.getElementById("posicion");
+// captuta la etiqueta input del id "posicion" donde se ingresa un numero que indica cuanto se avansa
+let output = document.getElementById("mostrando_text"); // output = salida 
+// captuta la etiqueta span del id "mostrando_text" donde se muestra el resultado
+
+
+let btn_encode = document.getElementById("Cifrando");
+let btn_decode = document.getElementById("Decifrando");
+
+/* target.addEventListener(tipo, listener[, useCapture]);Registra un evento a un objeto en específico (al button Cifrando)
+   ejm target.addEventListener(nombre del evento, es objeto o solo una function[, boolean: true o false por defecto]);*/
+btn_encode.addEventListener("click", function(event) {
+/* event.preventDefault() Cancela el evento, sin detener el resto del funcionamiento del evento es decir, 
+   puede ser llamado de nuevo. (para que la etiqueta <form>  nose direccione a la url)  */
+  event.preventDefault();
   
-  event.preventDefault();//para que la etiqueta form no se nose direccione a la url
-   let resul_encriptado = cipher.encode(document.getElementById("ingresando_text").value,document.getElementById("posicion").value);
+  // el objeto cipher , su metodo encode y sus (parametros)
+  let result_encode = cipher.encode(in_text.value,in_position.value);
+  output.innerHTML = result_encode;
 
-    document.getElementById("mostrando_text").innerHTML = resul_encriptado;
+});
 
-},false);
+btn_decode.addEventListener("click",function(event) {
+  event.preventDefault();
 
-document.getElementById("Decifrando").addEventListener("click",function(event){
- event.preventDefault();//
-  let resul_desencriptado = cipher.decode(document.getElementById("ingresando_text").value,document.getElementById("posicion").value);
+// el objeto cipher , su metodo decode y sus (parametros)
+  let result_decode = cipher.decode(in_text.value,in_position.value);
+  output.innerHTML = result_decode;
 
-     document.getElementById("mostrando_text").innerHTML = resul_desencriptado;
-
-},false);
+});
 
 
 
